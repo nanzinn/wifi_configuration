579937846c5854c0f086ce066eaa8c3996c4d7b8
xeroso <xeroso>
2021-05-27 09:25:45 -0300

    teste

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/dbws/Downloads/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/dbws/AndroidStudioProjects/Plugin/wifi_configuration/example"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_ROOT=/Users/xeroso/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/xeroso/development/wifi_configuration/example"
+export "FLUTTER_TARGET=/Users/xeroso/development/wifi_configuration/example/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/dbws/Downloads/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=flutter.inspector.structuredErrors%3Dtrue"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/xeroso/development/wifi_configuration/example/.dart_tool/package_config.json"
